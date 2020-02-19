0bc6207e84aabb25b905547f2bf4b9f1e338f1cb
Sang Min Park <9714park@gmail.com>
2020-02-19 09:55:26 -0500

    Updates

---
+++ precache-manifest.ca3f45b0ae7e8f14a87b3a7860b2457f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8815ccdd2d0871284a992dae37088a89",
+    "url": "/react-web-portfolio/index.html"
+  },
+  {
+    "revision": "6bf3541070369f025c8b",
+    "url": "/react-web-portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5494d0eb8e69899e2742",
+    "url": "/react-web-portfolio/static/js/2.12fc3383.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/react-web-portfolio/static/js/2.12fc3383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf3541070369f025c8b",
+    "url": "/react-web-portfolio/static/js/main.30749a84.chunk.js"
+  },
+  {
+    "revision": "2f3531d0ac64fcb7af5a",
+    "url": "/react-web-portfolio/static/js/runtime-main.15ac22b8.js"
+  }
+]);
